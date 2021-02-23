f50f7458d638a482bc6b17593cf8f80ffb79cc96
LexLive <76833027+LexLive@users.noreply.github.com>
2021-02-23 21:53:02 +0700

    Add files via upload

---
+++ Section_3_Printing.py
@@ -0,0 +1,67 @@
+print('Hello \nWorld\nI am Mostafa')
+print("There is", 1, "instructor not", 2 * 3)
+print('Let\'s go')
+print('Richness is not having many possessions, \nbut richness is being content with oneself.')
+
+#Homework 1 #: Guess the run-time output
+print("-" * 20, 'Homework 1')
+print('Practice', "Make Perfact.")
+print("Children must be taught\n  how tothink\n  NOT\n  what to think")
+print('2 * 3 * 4 * 5 * 10 = ')
+print(2 * 3 * 4 * 5 / 10)
+
+
+#Homework 2
+print("-" * 20, 'Homework 2')
+print('Print("Me")')
+print("Print('Me')")
+print("Print('Me\")")
+print("You will learn\n a LOT")
+
+#Homework 3
+print("-" * 20, 'Homework 3')
+print("*")
+print("* *")
+print("* * *")
+print("* * * *")
+print("* * * * *")
+
+#Homework 4
+print("-" * 20, 'Homework 4')
+print("       *")
+print("     * * *")
+print("    * * * * *")
+print("  * * * * * * *")
+print("* * * * * * * * *")
+
+#Homework 5
+print("-" * 20, 'Homework 5')
+print("        *")
+print("      * * *")
+print("    * * * * *")
+print("  * * * * * * *")
+print("* * * * * * * * *")
+print("  * * * * * * *")
+print("    * * * * *")
+print("      * * *")
+print("        *")
+#Homework 6
+print("-" * 20, 'Homework 6')
+print('Hi')
+print()
+print("I am mostafa")
+print(3 * 6)
+print("Wonderful", 'day')
+print("Wonderful " + "day")
+
+
+
+
+
+
+
+"""
+  Hello
+   World
+    I am Mostafa
+"""

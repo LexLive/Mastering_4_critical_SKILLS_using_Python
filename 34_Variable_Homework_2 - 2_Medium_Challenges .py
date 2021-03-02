04a6bc77e04e0a48c307bb16d8bc546752821326
LexLive <76833027+LexLive@users.noreply.github.com>
2021-03-02 13:51:37 +0700

    Add files via upload

---
+++ 34_Variable_Homework_2 - 2_Medium_Challenges .py
@@ -0,0 +1,61 @@
+#Homework 1: Guess Output
+'''
+num1 = 1
+num2 = 2
+
+num3 = num1 + num2
+num1 = num2
+num2 = num3
+
+num3 = num1 + num2
+num1 = num2
+num2 = num3
+
+num3 = num1 + num2
+num1 = num2
+num2 = num3
+
+num3 = num1 + num2
+num1 = num2
+num2 = num3
+
+num3 = num1 + num2
+num1 = num2
+num2 = num3
+
+num3 = num1 + num2
+num1 = num2
+num2 = num3
+
+
+print(num3)
+'''
+
+#Homework 2: Swapping 2 numbers!
+
+'''
+num1, num2 = map(int, input().split())
+
+print(num2, num1)
+'''
+
+
+'''
+one1, two2 = map(int, input().split())
+
+#creat temporary to hold one1
+three3 = one1 
+
+#give one1 value of two2
+one1 = two2
+
+#give two2 the temp value
+two2 = three3
+
+print(one1, two2)
+
+'''
+
+
+
+
